8e022879e7b87de1f78207cd3b4f093fc2936808
Wai Chan <getwaichan@gmail.com>
2020-03-20 16:53:14 -0400

    completed module 3

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { dir } = require('console');
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.urlencoded,  { extended: true} );
+app.use(express.urlencoded(  { extended: true}) );
 
 
 let accountData="";
@@ -34,8 +35,21 @@ app.get('/checking', (req,res) => res.render( 'account', { account: accounts.che
 app.get('/credit', (req,res) => res.render( 'account', { account: accounts.credit }));
 app.get('/profile', (req,res) => res.render('profile', { user: users[0] }));
 app.get('/transfer', (req,res) => res.render('transfer'));
-app.post('/transfer', (req,res) => res.render('transfer',
-        accoubts[req.body.to].balance = accounts[req.body.from].balance.parseInt() - req.body.amount.parseInt() 
-));
+app.post('/transfer', (req,res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
+    accountsJSON = JSON.stringify(accounts, null,4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', { message: 'Transfer Completed'});
+});
+app.get('/payment', (req,res) => res.render('payment', { account: accounts.credit }));
+
+app.post('/payment', (req,res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount,10); 
+    accountsJSON = JSON.stringify(accounts, null,4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');      
+    res.render('payment'), { message: 'Payment Successful', account: accounts.credit }
+});
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
